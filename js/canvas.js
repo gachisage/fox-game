a26fa2d6e6eab9e40f8ee870b4fe3ce4982443b5
Yablonev <68510481+Yablonev@users.noreply.github.com>
2023-03-26 22:20:49 +0100

    !

--- js/canvas.js
+++ js/canvas.js
@@ -1,13 +1,15 @@
 const $ = (selector) => document.querySelector(selector)
+const $$ = (selector) => [...document.querySelectorAll(selector)]
 
 const canvas = $('#canvas')
+canvas.width = '528'
+canvas.height = '900'
 const ctx = canvas.getContext('2d')
 ctx.imageSmoothingEnabled = false
 
-canvas.width = document.body.clientWidth
-canvas.height = document.body.clientHeight
+const key = `FG12x403df`
 
-const promo_code = 'You have reached the KEY: FG12x403df'
+const promo_code = `You have reached the KEY: ${key}`
 const count_for_win = 560
 
 const ground = $('.ground')
@@ -18,12 +20,12 @@ const many_trees = $('.many_trees')
 const sky = $('.sky')
 const moon = $('.moon')
 const fox_death = $('.fox_death')
-
+const fox = $('.fox')
+const count = $('#count')
 const snowman = $('.snowman')
-const snowmans = []
 
 let gameMove = true
-let startGame = false
+let isGameStarted = false
 let startMoveBack = false
 let isJumping = false
 let isJumpingAmimation = false
@@ -34,73 +36,43 @@ let spaceStop = true
 let stopAnimationFoxDeath = false
 let checkPromo = false
 
-const fox = $('.fox')
-
-const count = $('#count')
 let countValue = 0
-count.innerHTML = '0'
+count.innerHTML = countValue
 
+// Animation fox
+let currentAnimation = null
 
-// Adptive
-let width_config
-let speed
-
-if (document.body.clientWidth < 2000 && document.body.clientWidth > 1000) {
-  width_config = {
-    widthImages         : canvas.width,
-    groundPosY          : canvas.height - 126,
-    many_treesPosY      : canvas.height - 126 - 130,
-    treePosY            : canvas.height - 126 - 120,
-    mountainsPosY       : canvas.height - 126 - 120 - 160,
-    back_mountainsPosY  : canvas.height - 126 - 120 - 100,
-    moonPosX            : canvas.width / 2,
-    fox_posY            : canvas.height - 126 - 30,
-    fox_posX            : canvas.width / 14,
-    snowman_posY        : canvas.height - 126 - 30,
-    jumpHeight          : 130,
-    adaptHeightSnowman  : 1,
-    collisionHeight     : 50,
-    fox_height          : 71,
-    fox_width           : 170,
-    speedJumpPX         : 2.3,
-  }
-  speed = {
-    groundLayer   : 3,
-    treeLayer     : 2.5,
-    manyTreeLayer : 2,
-    mountLayer    : 0.8,
-    backMountLayer: 0.4,
-    snowman       : 3,
-  }
+const foxConfig = {
+  jump: false,
+  timeJump: 2000,
 }
 
-if (document.body.clientWidth < 1000) {
-  width_config = {
-    widthImages         : 1000,
-    groundPosY          : canvas.height - 126,
-    many_treesPosY      : canvas.height - 126 - 130,
-    treePosY            : canvas.height - 126 - 120,
-    mountainsPosY       : canvas.height - 126 - 120 - 160,
-    back_mountainsPosY  : canvas.height - 126 - 120 - 100,
-    moonPosX            : canvas.width / 2,
-    fox_posY            : canvas.height - 126,
-    fox_posX            : canvas.width / 14,
-    snowman_posY        : canvas.height - 126,
-    jumpHeight          : 80,
-    adaptHeightSnowman  : 0.5,
-    collisionHeight     : 30,
-    fox_height          : 71 * 0.5,
-    fox_width           : 170 * 0.5,
-    speedJumpPX         : 1.5,
-  }
-  speed = {
-    groundLayer   : 2,
-    treeLayer     : 1,
-    manyTreeLayer : 0.7,
-    mountLayer    : 0.3,
-    backMountLayer: 0.1,
-    snowman       : 2,
-  }
+const width_config = {
+  widthImages         : canvas.width,
+  groundPosY          : canvas.height - 110,
+  many_treesPosY      : canvas.height - 126 - 130,
+  treePosY            : canvas.height - 126 - 120,
+  mountainsPosY       : canvas.height - 126 - 120 - 160,
+  back_mountainsPosY  : canvas.height - 126 - 120 - 100,
+  moonPosX            : canvas.width / 2,
+  fox_posY            : canvas.height - 126 - 30,
+  fox_posX            : canvas.width / 14,
+  snowman_posY        : canvas.height - 126 - 30,
+  jumpHeight          : 130,
+  adaptHeightSnowman  : 1,
+  collisionHeight     : 50,
+  fox_height          : 71,
+  fox_width           : 170,
+  speedJumpPX         : 2.3,
+}
+
+const speed = {
+  groundLayer   : 3,
+  treeLayer     : 2.5,
+  manyTreeLayer : 2,
+  mountLayer    : 0.8,
+  backMountLayer: 0.4,
+  snowman       : 3,
 }
 
 const wh = canvas.width / canvas.height * 1.5
@@ -110,63 +82,11 @@ const treePosY = groundPosY - tree.height * wh
 const mountainsPosY = many_treesPosY - mountains.height * wh * 0.8
 const back_mountainsPosY = many_treesPosY - mountains.height * wh * 0.8
 
-if (document.body.clientWidth > 2000) {
-  width_config = {
-    widthImages         : canvas.width,
-    groundPosY          : groundPosY,
-    many_treesPosY      : many_treesPosY,
-    treePosY            : treePosY,
-    mountainsPosY       : mountainsPosY,
-    back_mountainsPosY  : back_mountainsPosY,
-    moonPosX            : canvas.width / 2,
-    fox_posY            : groundPosY - fox.height,
-    fox_posX            : canvas.width / 14,
-    snowman_posY        : groundPosY - fox.height,
-    jumpHeight          : 180,
-    adaptHeightSnowman  : 1.8,
-    collisionHeight     : 80,
-    fox_height          : 71 * 1.8,
-    fox_width           : 170 * 1.8,
-    speedJumpPX         : 4,
-  }
-  speed = {
-    groundLayer   : 10,
-    treeLayer     : 4,
-    manyTreeLayer : 3,
-    mountLayer    : 1,
-    backMountLayer: 0.8,
-    snowman       : 10,
-  }
-}
-
 const death_config = {
  smowPosX: []
 }
 
-function start_Game() {
-  if (!startGame) {
-    countInt()
-    startMoveBack = true;
-    startGame = true;
-    checkVariableTextToStart()
-  }
-}
-
-document.addEventListener('click', start_Game)
-
-updateSizeDoc()
-jumpFox()
-createTextToStart()
-
-
-
-function updateSizeDoc() {
-  canvas.width = document.body.clientWidth
-  canvas.height = document.body.clientHeight
-}
-
-
-class Layer{
+class Layer {
   constructor (image, movSpeed, posY, img_width, height) {
     this.x = 0
     this.y = posY || 0
@@ -179,18 +99,8 @@ class Layer{
   }
 
   draw() {
-    if (document.body.clientWidth > 1000 && document.body.clientWidth < 2001) {
-      ctx.drawImage(this.image, this.x, this.y, canvas.width, this.image.height),
-      ctx.drawImage(this.image, this.x2, this.y, canvas.width, this.image.height)
-    }
-    if (document.body.clientWidth > 2000) {
-      ctx.drawImage(this.image, this.x, this.y, canvas.width, this.image.height * canvas.width / canvas.height * 1.5),
-      ctx.drawImage(this.image, this.x2, this.y, canvas.width, this.image.height * canvas.width / canvas.height * 1.5)
-    }
-    if (document.body.clientWidth < 1000) {
-      ctx.drawImage(this.image, this.x, this.y)
-      ctx.drawImage(this.image, this.x2, this.y)
-    }
+    ctx.drawImage(this.image, this.x, this.y)
+    ctx.drawImage(this.image, this.x2, this.y)
   }
 
   update() {
@@ -200,7 +110,8 @@ class Layer{
     else this.x2 -= this.speed
   }
 }
-class Snowman{
+
+class Snowman {
   constructor () {
     this.x = canvas.width
     this.y = width_config.snowman_posY
@@ -227,7 +138,8 @@ class Snowman{
     if (this.x < 0 - 20) this.x = canvas.width
   }
 }
-class Fox{
+
+class Fox {
   constructor () {
     this.x = width_config.fox_posX
     this.y = width_config.fox_posY
@@ -240,7 +152,7 @@ class Fox{
   }
 
   draw() {
-    if(!stopAnimationFoxDeath){
+    if (!stopAnimationFoxDeath) {
       if (document.body.clientWidth > 2000) {
         ctx.drawImage(this.image, this.x, this.y, this.image.width * width_config.adaptHeightSnowman, this.height)
       }
@@ -281,68 +193,45 @@ const moveObj = [backMountLayer, mountLayer, manyTreeLayer, groundLayer, treeLay
 const foxLayer = new Fox()
 const snower = new Snowman(2, 300)
 
-
-document.addEventListener('click', () => {
-  if(isJumping){
-    if (!foxLayer.jumping && gameMove) {
-      foxLayer.jumping = true
-    }
+function startGame() {
+  $('.modal.start')?.remove()
+  if (!isGameStarted) {
+    countInt()
+    startMoveBack = true
+    isGameStarted = true
   }
-  if(!isJumping){
-    isJumping = true
-  }
-
-})
-
-document.addEventListener('keydown', (event) => {
-  if (event.keyCode == 32 || event.keyCode == 38) {
-    if (!foxLayer.jumping) {
-      foxLayer.jumping = true
-    }
-  }
-})
-// let checkGameOver = document
-document.addEventListener('click', () => {
-  if (!gameMove && document.querySelector('.game_over')) {
-    reload()
-  }
-})
-
-function reload(){
-  location.reload()
 }
 
 function collision() {
   if (foxLayer.x < snower.x &&
-     foxLayer.x + foxLayer.width * width_config.adaptHeightSnowman/1.3 > snower.x &&
-     foxLayer.y + width_config.collisionHeight > snower.y && gameMove
-    ) {
-        const promoCode = document.createElement('H2')
-        document.body.append(promoCode)
-        promoCode.classList.add('game_over')
-        promoCode.classList.add('modal')
-        promoCode.innerHTML = `GAME OVER <br> click for a new game`
-        countValue = 0
-        fox.src = `fox/death_5.png`
-        snower.x = canvas.width
-        checkGameOver = $('.game_over')
-        dafthAnimation = true
-        if (currentAnimation) {
-          cancelAnimationFrame(currentAnimation)
-          currentAnimation = null
-        }
-        if(dafthAnimation){
-          sprintsForMove(6, 100, '.fox', 'fox/death_', '.png')
-          setTimeout(() => {
-            // stopAnimationFox = true
-            stopAnimationFoxDeath = true
-          }, 500)
-        }
+      foxLayer.x + foxLayer.width * width_config.adaptHeightSnowman / 1.3 > snower.x &&
+      foxLayer.y + width_config.collisionHeight > snower.y && gameMove) {
+
+      $('.modal.game_over').style.display = 'block'
+      countValue = 0
+      fox.src = `fox/death_5.png`
+      snower.x = canvas.width
+      dafthAnimation = true
+
+      if (currentAnimation) {
+        cancelAnimationFrame(currentAnimation)
+        currentAnimation = null
+      }
+
+      if (dafthAnimation) {
+        sprintsForMove(6, 100, '.fox', 'fox/death_', '.png')
+        setTimeout(() => {
+          // stopAnimationFox = true
+          stopAnimationFoxDeath = true
+        }, 500)
+      }
+
     return gameMove = false
   }
 }
+
 function animate() {
-  if(!checkPromo){
+  if (!checkPromo) {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.drawImage(sky, 0, 0, canvas.width, canvas.height)
     ctx.drawImage(moon, width_config.moonPosX, 0, moon.width * wh * 0.8, moon.height * wh * 0.8)
@@ -353,9 +242,11 @@ function animate() {
     treeLayer.draw()
 
     groundLayer.draw()
-   if(stopAnimationFoxDeath){
-    ctx.drawImage(fox_death, width_config.fox_posX, width_config.fox_posY, width_config.fox_width ,width_config.fox_height)
-   }
+
+    if (stopAnimationFoxDeath) {
+      ctx.drawImage(fox_death, width_config.fox_posX, width_config.fox_posY, width_config.fox_width ,width_config.fox_height)
+    }
+
     if (gameMove) {
       moveObj.forEach(item => {
         item.update()
@@ -367,7 +258,7 @@ function animate() {
         snower.draw()
       }
     }
-    if(!stopAnimationFox){
+    if (!stopAnimationFox) {
       foxLayer.draw()
     }
     foxLayer.jump()
@@ -376,12 +267,6 @@ function animate() {
     requestAnimationFrame(animate)
   }
 
-}
-animate()
-
-
-function rand(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 // Promo code
@@ -396,108 +281,35 @@ function countInt() {
   }, 100)
 }
 
-
-
 function promoСode() {
   if (countValue > count_for_win && countValue < count_for_win + 2) {
-    createPromoCode()
+    $('.modal.promocode').style.display = 'block'
     checkPromo = true
-    $('#buttons').style.display = 'flex'
   }
 }
 
-function checkVariablePromo() {
-  const promoCode = $('.promo')
-  document.body.removeChild(promoCode)
-}
-
-function createPromoCode() {
-  const promoCode = document.createElement('h2')
-  document.body.append(promoCode)
-  promoCode.classList.add('promo')
-  promoCode.classList.add('modal')
-  promoCode.innerHTML = promo_code
-}
-
-function checkVariableTextToStart() {
-  const promoCode = $('.modal')
-  document.body.removeChild(promoCode)
-}
-
-function createTextToStart() {
-  const TextToStart = document.createElement('h2')
-  document.body.append(TextToStart)
-  TextToStart.classList.add('modal')
-  TextToStart.innerHTML = 'click to start'
-}
-
-
-window.addEventListener('resize', () => {
-  updateSizeDoc()
-  // drawCanvas()
-})
-
-// Animation fox
-let currentAnimation = null
-let fox_config = {
-  jump: false,
-  timeJump: 2000,
-}
-function srartRun() {
-  if (gameMove) sprintsForMove(5, 100, '.fox', 'fox/run_', '.png')
-  if (!gameMove) sprintsForMove(6, 100, '.fox', 'fox/death_', '.png')
-}
-srartRun()
-
-function jumpFox() {
-  window.addEventListener('keydown', (event) => {
-
-    if (event.keyCode == 32 && spaceStop || event.keyCode == 38 && spaceStop) {
-      spaceStop = false
-      fox_config.jump = true
-      switchAnimation()
-      setTimeout(() => {
-        spaceStop = true
-      }, fox_config.timeJump - 10)
-    }
-
-  })
-
-  window.addEventListener('click', () => {
-    if(isJumpingAmimation && spaceStop){
-      spaceStop = false
-      fox_config.jump = true
-      switchAnimation()
-      setTimeout(() => {
-        spaceStop = true
-      }, fox_config.timeJump - 10)
-    }
-    if(!isJumpingAmimation){
-      isJumpingAmimation = true
-    }
-  })
-}
-
 function switchAnimation() {
-  if (!dafthAnimation){
-    console.log('работает!');
+  if (!dafthAnimation) {
+    console.log('Работает!')
+
     if (currentAnimation) {
       cancelAnimationFrame(currentAnimation)
       currentAnimation = null
     }
-    if (fox_config.jump && gameMove) {
+
+    if (foxConfig.jump && gameMove) {
       sprintsForMove(9, 200, '.fox', 'fox/jump_', '.png')
       setTimeout(() => {
-        fox_config.jump = false
+        foxConfig.jump = false
         switchAnimation()
-      }, fox_config.timeJump)
-    }
-    else if(!fox_config.jump && !dafthAnimation|| !currentAnimation && !dafthAnimation) {
+      }, foxConfig.timeJump)
+    } else if (!foxConfig.jump && !dafthAnimation|| !currentAnimation && !dafthAnimation) {
       sprintsForMove(5, 100, '.fox', 'fox/run_', '.png')
     }
   }
-  if (dafthAnimation){
-    console.log('работает смерть!')
+
+  if (dafthAnimation) {
+    console.log('Работает смерть!')
     sprintsForMove(6, 100, '.fox', 'fox/death_', '.png')
   }
 }
@@ -518,16 +330,81 @@ function sprintsForMove(count, int, selector, path1, path2) {
     }
     currentAnimation = requestAnimationFrame(switchImage)
   }
+
   currentAnimation = requestAnimationFrame(switchImage)
 }
 
-//buttons copy and new Game
-const text = 'FG12x403df'
+// Start
+
+onLoad()
+
+function onLoad() {
+  if (gameMove) sprintsForMove(5, 100, '.fox', 'fox/run_', '.png')
+  if (!gameMove) sprintsForMove(6, 100, '.fox', 'fox/death_', '.png')
+  animate()
+}
+
+// Events
+document.addEventListener('click', () => {
+  startGame()
+
+  if (isJumpingAmimation && spaceStop) {
+    spaceStop = false
+    foxConfig.jump = true
+    switchAnimation()
+    setTimeout(() => {
+      spaceStop = true
+    }, foxConfig.timeJump - 10)
+  }
+
+  if (!isJumpingAmimation) isJumpingAmimation = true
+
+  if (isJumping) {
+    if (!foxLayer.jumping && gameMove) {
+      foxLayer.jumping = true
+    }
+  } else isJumping = true
+
+  if (!gameMove && document.querySelector('.game_over')) {
+    reload()
+  }
+})
+
+document.addEventListener('keydown', (event) => {
+  startGame()
+
+  if (event.keyCode == 32 && spaceStop || event.keyCode == 38 && spaceStop) {
+    spaceStop = false
+    foxConfig.jump = true
+    switchAnimation()
+    setTimeout(() => {
+      spaceStop = true
+    }, foxConfig.timeJump - 10)
+  }
+
+  if (event.keyCode == 32 || event.keyCode == 38) {
+    if (!foxLayer.jumping) {
+      foxLayer.jumping = true
+    }
+  }
+})
+
+// Utils
+
+function reload() {
+  location.reload()
+}
+
+function rand(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+// Buttons copy and new Game
 const copyContent = async () => {
   try {
-    await navigator.clipboard.writeText(text);
-    console.log('Content copied to clipboard');
+    await navigator.clipboard.writeText(key)
+    console.log('Content copied to clipboard')
   } catch (err) {
-    console.error('Failed to copy: ', err);
+    console.error('Failed to copy: ', err)
   }
 }
